5c0dba751a7644f10947b11e7d31ece0810a0964
Paulo Henrique Duarte <igrsysten@gmail.com>
2023-05-13 02:00:04 -0300

    Update layout.php

--- app/Views/usuarios/panel/layout.php
+++ app/Views/usuarios/panel/layout.php
@@ -6,6 +6,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.plyr.io/3.6.2/plyr.css" />
+
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
@@ -99,8 +101,9 @@
         <div class="row">
             <div class="col-lg-8 offset-lg-2 text-center">
                 <?php if ($stream_pt) : ?>
-                    <div class="ratio ratio-16x9">
-                        <iframe src="<?= $stream_pt ?>" title="YouTube video" allowfullscreen></iframe>
+                    <div class="ratio ratio-16x9 plyr__video-embed" id="player">
+                        <iframe src="<?= $stream_pt ?>" allowfullscreen allowtransparency allow="autoplay">
+                        </iframe>
                     </div>
                 <?php else : ?>
                     <div class="card">
@@ -134,6 +137,12 @@
             </div>
         </nav>
     </footer>
+
+    <script src="https://cdn.plyr.io/3.6.2/plyr.js"></script>
+    <script>
+        const player = new Plyr('#player');
+    </script>
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
