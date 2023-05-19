a48ac0ec6c99cf8be5fef4e077413aba5c42522e
Paulo Henrique Duarte <igrsysten@gmail.com>
2023-05-19 01:54:29 -0300

    Update header.php

--- app/Views/newSuper/template/header.php
+++ app/Views/newSuper/template/header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="pt-BR" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-layout-mode="dark" data-body-image="img-1" data-preloader="disable">
+<html lang="pt-BR" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-layout-mode="light" data-body-image="img-1" data-preloader="disable">
 
 <head>
 
