1d4bfd40e7d9506b2edf9d1d49163e988da7d574
Paulo Henrique Duarte <igrsysten@gmail.com>
2023-05-20 06:34:38 -0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo cd /www/wwwroot/conect.app/scripts
+cd /www/wwwroot/conect.app/scripts
 
 sudo chmod +x after_install.sh
 
-sudo cd /www/wwwroot/conect.app
+cd /www/wwwroot/conect.app
 
 # Outras ações pós-instalação
-composer install --no-interaction --optimize-autoloader
+composer install --no-dev
