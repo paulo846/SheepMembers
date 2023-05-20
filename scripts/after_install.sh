f9620d75082932d63ad7693674cd8aa3e0b90b23
Paulo Henrique Duarte <igrsysten@gmail.com>
2023-05-20 09:36:11 -0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,6 +8,6 @@ sudo chmod +x after_install.sh
 cd /www/wwwroot/conect.app
 
 # Outras ações pós-instalação
-sudo composer update --no-dev
+composer install --no-dev
 
 sudo chmod -R 777 /www/wwwroot/conect.app/writable
