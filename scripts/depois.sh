ae09811e95bcebd9b2dd79632eb30b13abf3a8b7
Paulo Henrique Duarte <igrsysten@gmail.com>
2023-05-20 03:41:50 -0300

    Update depois.sh

--- scripts/depois.sh
+++ scripts/depois.sh
@@ -6,5 +6,5 @@ PROJECT_DIR="/www/wwwroot/conect.app"
 # Altera para o diretório do projeto
 cd "$PROJECT_DIR"
 
-# Executa o composer no modo de produção do CodeIgniter 4
-sudo composer install --no-dev --optimize-autoloader --no-interaction -y
+# Executa o composer no modo de produção do CodeIgniter 4 com permissões de root
+sudo -E composer install --no-dev --optimize-autoloader --no-interaction
