13d8fa941034b2a70962dbe33351a136c8e9857f
Paulo Henrique Duarte <igrsysten@gmail.com>
2023-05-20 04:25:45 -0300

    Update Aws.php

--- app/Config/Aws.php
+++ app/Config/Aws.php
@@ -6,11 +6,5 @@ use CodeIgniter\Config\BaseConfig;
 
 class Aws extends BaseConfig
 {
-    public $credentials = [
-        'key'    => 'AKIAZXNJYHISLK6MOI6P',
-        'secret' => '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',
-    ];
     
-    public $region = 'us-east-1';
-    public $bucket = 'sheepmembers';
 }
