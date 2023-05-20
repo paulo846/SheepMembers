c183d07d7715554d66c6f7a2606776ab71507d4e
Paulo Henrique Duarte <igrsysten@gmail.com>
2023-05-20 05:01:35 -0300

    Update depois.sh

--- scripts/depois.sh
+++ scripts/depois.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-# Define o diretório raiz do projeto
-PROJECT_DIR="/www/wwwroot/conect.app"
-
-# Altera para o diretório do projeto
-cd "$PROJECT_DIR"
-
-# Executa o composer no modo de produção do CodeIgniter 4 com permissões de root
-composer install --no-dev --optimize-autoloader --no-interaction
-
+cd "/www/wwwroot/conect.app"
+composer install
 sudo chmod -R 777 /www/wwwroot/conect.app/writable
