606e6929c26bca1ce5b6d7d2ba42e13a0b1dec34
Paulo Henrique Duarte <igrsysten@gmail.com>
2023-05-20 02:51:36 -0300

    Atualização

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -57,7 +57,8 @@ class Filters extends BaseConfig
      * with a method you don’t expect could bypass the filter.
      */
     public array $methods = [
-        'post' => ['throttle']
+        'post' => ['throttle'],
+        'get'  => ['throttle']
     ];
 
     /**
