e094496c662b741095f945d79c74cb5467e635f4
Paulo Henrique Duarte <igrsysten@gmail.com>
2023-06-01 23:23:56 -0300

    Update Clients.php

--- app/Config/Routes/Clients.php
+++ app/Config/Routes/Clients.php
@@ -5,7 +5,7 @@ $routes->get('filme/(:any)', 'Home::filme/$1', ['filter' => 'loggedclient']);
 $routes->get('perfil', 'Home::perfil', ['filter' => 'loggedclient']);
 //$routes->get('/', 'Home::index');
 
-$routes->get('/teste', 'Teste::index');
+$routes->post('/teste', 'Teste::index');
 
 $routes->get('/mostrar', 'Teste::mostrar');
 
