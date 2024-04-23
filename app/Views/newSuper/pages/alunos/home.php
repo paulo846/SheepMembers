868b7541d925f64e1ba17ccc2c957c7570914feb
Paulo Henrique Duarte <igrsysten@gmail.com>
2024-04-23 03:02:14 -0300

    Update home.php

--- app/Views/newSuper/pages/alunos/home.php
+++ app/Views/newSuper/pages/alunos/home.php
@@ -97,7 +97,7 @@ Listados: <?= count($cliente) ?>
                                                 if (isset($row['updated_at']) && isset($row['created_at'])) {
 
                                                     // Assume current time is: March 10, 2017 (America/Chicago)
-                                                    $time = Time::parse($row['updated_at'], 'America/sao_paulo');
+                                                    $time = Time::parse($row['updated_at']);
 
                                                     echo $time->humanize(); // 1 year ago
                                                 }
