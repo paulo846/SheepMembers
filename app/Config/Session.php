1948767b27d4aac6bad8d5a74190f747cd77af4b
Paulo Henrique Duarte <igrsysten@gmail.com>
2024-04-23 03:10:31 -0300

    Update Session.php

--- app/Config/Session.php
+++ app/Config/Session.php
@@ -21,7 +21,7 @@ class Session extends BaseConfig
      *
      * @phpstan-var class-string<BaseHandler>
      */
-    public string $driver = 'CodeIgniter\Session\Handlers\RedisHandler';
+    public string $driver = FileHandler::class;
 
     /**
      * --------------------------------------------------------------------------
